bc93c5e7bdb0fd61390cfc058b099c47f05adfdd
tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-08-07 20:44:47 +0900

    candy.ino

---
+++ ARDUINO/candy/candy.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+
+byte mac[] = { 0x80, 0xA2, 0xDA, 0x0F, 0x88, 0x32 };
+IPAddress ip( 172, 16, 14, 200 );
+unsigned int loclaPort = 4000;
+
+char packetBuffer[ UDP_TX_PACKET_MAX_SIZE ];
+EthernetUDP Udp;
+
+void setup() {
+    Serial.begin( 9600 );
+    Ethernet.begin( mac, ip );
+    Udp.begin( localPort );
+}
+
+int get_commnad() {
+    if( Udp.parsePacket() ) {
+        Udp.read( packetBuffer, UDP_TX_PACKET_MAX_SIZE );
+
+        if( packetBuffer == "GO" ) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
+
+void action() {
+    Serial.println("GO");
+}
+
+void loop() {
+    int return_num = get_command();
+    if (return_num) {
+        action();
+    }
+}
