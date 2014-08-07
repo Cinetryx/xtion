2562f75f36a7623aac978e2c5b86a8f75b7e9a40
tokunn <kaseikappatokunn2@yahoo.co.jp>
2014-08-07 20:46:36 +0900

    candy update

--- ARDUINO/candy/candy.ino
+++ ARDUINO/candy/candy.ino
@@ -4,7 +4,7 @@
 
 byte mac[] = { 0x80, 0xA2, 0xDA, 0x0F, 0x88, 0x32 };
 IPAddress ip( 172, 16, 14, 200 );
-unsigned int loclaPort = 4000;
+unsigned int localPort = 4000;
 
 char packetBuffer[ UDP_TX_PACKET_MAX_SIZE ];
 EthernetUDP Udp;
@@ -15,7 +15,7 @@ void setup() {
     Udp.begin( localPort );
 }
 
-int get_commnad() {
+int get_command() {
     if( Udp.parsePacket() ) {
         Udp.read( packetBuffer, UDP_TX_PACKET_MAX_SIZE );
 
